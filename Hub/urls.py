eb91674e77f75e09b10ef39688ce75af0d392836
FirasKhadraoui <firas.khadraoui@esprit.tn>
2022-03-03 00:34:58 +0100

    AddCoach

--- Hub/urls.py
+++ Hub/urls.py
@@ -1,6 +1,6 @@
 import imp
 from django.urls import path
-from .views import HomePage, ListCoach, details_coach, list_project
+from .views import Coach_add, HomePage, ListCoach, details_coach, list_project
 
 
 urlpatterns=[
@@ -9,4 +9,7 @@ urlpatterns=[
     #path('Coach',details_coach,name="Hub_Coach_details"), #id statique dans la fonction
     path('Coach/<int:id>',details_coach,name="Hub_Coach_details"),
     path('listProjet',list_project,name="Hub_List_projet"),
+    #path('listsecond', ProjectListView.as_view(),name="Hub_Project_List_Second"),
+    path('coachadd',Coach_add,name="Coach_add"),
+    #path('coachadd',Coach_addForm,name="Coach_add"),
 ]
