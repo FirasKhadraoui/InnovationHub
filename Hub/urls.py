ef0015c41534ddc6e7c8c8cb5668740a090840e6
FirasKhadraoui <firas.khadraoui@esprit.tn>
2022-03-02 21:10:41 +0100

    viewsHub2

--- Hub/urls.py
+++ Hub/urls.py
@@ -1,9 +1,12 @@
 import imp
 from django.urls import path
-from .views import HomePage, ListCoach
+from .views import HomePage, ListCoach, details_coach, list_project
 
 
 urlpatterns=[
     path('home',HomePage,name="Hub_home"),
     path('listCoach',ListCoach,name="Hub_Coach_list"),
+    #path('Coach',details_coach,name="Hub_Coach_details"), #id statique dans la fonction
+    path('Coach/<int:id>',details_coach,name="Hub_Coach_details"),
+    path('listProjet',list_project,name="Hub_List_projet"),
 ]
--- Hub/views.py
+++ Hub/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Coach
+from .models import Coach, Projet
 
 # Create your views here.
 def HomePage(request):
@@ -15,3 +15,27 @@ def ListCoach(request):
             'list_Coach':List
         } 
     )
+
+def details_coach(request,id):
+    coach=Coach.objects.get(id=id)
+    return render( 
+        request,
+        'Hub/details.html',
+        {
+            'coach': coach
+        } 
+    )
+
+def list_project(request) :
+    list= Projet.objects.all()
+    return render(
+        request,
+        'Hub/project_list.html',
+        {
+            'list_project':list
+        }
+    )
+
+# class ProjectListView(ListView) :
+#     model=Projet
+#     template_name='Hub/projet_list.html'
